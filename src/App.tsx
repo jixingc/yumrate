65ca666bb0efaacbd3926916b80f7b9e9777b9c5
Xingchen Ji <xingchen.ji@momenta.ai>
2026-04-30 17:11:35 +0800

    fix: use HashRouter instead of BrowserRouter to prevent white screen on Android Capacitor navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { DeckView } from './pages/DeckView';
 import { EntryView } from './pages/EntryView';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<DeckView />} />
         <Route path="/entry" element={<EntryView />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
